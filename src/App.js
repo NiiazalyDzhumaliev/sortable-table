5086f96929832fc9f5779ebd2fbc23c85daa5cf7
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2021-03-25 14:40:31 +0600

    Add Table component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-function App() {
+import Table from './components/Table';
+
+const App = () => {
   return (
     <div className="App">
       <h1>Sortable Table</h1>
+      <Table />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Table.js
@@ -0,0 +1,18 @@
+const Table = () => {
+  return (
+    <table>
+      <caption>Результаты</caption>
+      <thead>
+        <tr>
+          <th>Место</th>
+          <th>Имя</th>
+          <th>Попадания</th>
+          <th>Скорострельность</th>
+        </tr>
+      </thead>
+      <tbody></tbody>
+    </table>
+  );
+};
+
+export default Table;
